1fc8a987c88a8fc7ca94c5ad29b41be4d3c515ec
msjin123 <158466791+msjin123@users.noreply.github.com>
2026-02-20 22:44:50 +0900

    피갤컵 3번 문제
    대회할때는 폭발은 1번만 일어나니 폭발/폭발 x로 나누기, 차분배열을 몰라서 24점만 받은 문제
    
    개인적으로 매우 좋은 문제? 라는 생각이 들었음
    기댓값의 선형성(전체의 기댓값 = 각각의 기댓값들의 합), 소인수분해(에라토스테네스의 체 알고리즘 이용해서 시간줄이기), 
    같은수를 소인수분해를 2번 할수는 없다는 것, 누적 합(차분배열 트릭) 을 전부 이용하는 문제임

---
+++ gold/35263_폭탄의 악마.py
@@ -0,0 +1,54 @@
+import sys
+input=sys.stdin.readline
+
+import math as m
+
+n,m=map(int,input().split())
+lst=list(map(int,input().split())) # 원래
+lst2=[] # 소인수 기댓값 
+for a in range(0,n):
+    b=lst[a]
+    
+    # 소인수분해하고, 소인수들 추출 (arr에 추출)
+    arr=set()
+    divnum=2
+    while(divnum<=int(b**0.5)):
+        if (b%divnum==0):
+            arr.add(divnum)
+            b=b//divnum
+        else:
+            divnum+=1
+    if (b>1):
+        arr.add(b)
+
+    average=sum(arr)/len(arr)
+    lst2.append(average) # lst2[a]=average
+# -------------------------------------------
+# ck=[False] * (n+1)
+# for _ in range(m):
+#     i,j=map(int,input().split())
+#     for num in range(i,j+1):
+#         ck[num]=True
+
+diff=[0] * (n+2)
+for _ in range(m):
+    i,j=map(int,input().split())
+    diff[i]+=1
+    diff[j+1]-=1
+
+
+# ---------------------------------------
+
+# (참고) cover이 음수는 될 수 없다
+ans=0
+cover=0
+for a in range(0,n):
+    cover+=diff[a+1]
+    if (cover>0):
+        ans+=lst2[a]
+    else:
+        ans+=lst[a]
+
+
+            
+print(ans)
